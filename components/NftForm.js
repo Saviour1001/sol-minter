93410ddd489f8bb293f30984758246643e2e8bc5
Anurag Gupta <anuraggupta5086@gmail.com>
2022-02-04 20:46:22 +0530

    nft form added

--- components/NftForm.js
+++ components/NftForm.js
@@ -1,48 +1,120 @@
-import React from "react";
-import { Form } from "web3uikit";
+import React, { useState } from "react";
+import { Input, TextArea, Button } from "web3uikit";
+import { actions, NodeWallet } from "@metaplex/js";
+import {
+  clusterApiUrl,
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+} from "@solana/web3.js";
+import { useMoralis } from "react-moralis";
 
 function NftForm() {
+  const { Moralis } = useMoralis();
+  const [nftName, setNFTName] = useState();
+  const [nftDesccriptiom, setNFTDescription] = useState();
+
+  async function uploadAndMintNFT() {
+    // Storing the file
+
+    const keypair = Keypair.generate();
+    let user = Moralis.User.current();
+
+    const fileInput = document.getElementById("file");
+    const data = fileInput.files[0];
+
+    const imageFile = new Moralis.File(data.name, data);
+    await imageFile.saveIPFS();
+
+    // Storing the metadata
+
+    const imageURI = imageFile.ipfs();
+
+    const metadata = {
+      name: nftName,
+      symbol: "MLH",
+      description: nftDesccriptiom,
+      image: imageURI,
+      seller_fee_basis_points: 0,
+      properties: {
+        files: [
+          {
+            uri: imageURI,
+            type: "image/jpeg",
+          },
+        ],
+        category: "image",
+        creators: [
+          {
+            address: keypair.publicKey,
+            verified: true,
+            share: 100,
+          },
+        ],
+      },
+    };
+    const metadataFile = new Moralis.File("metadata.json", {
+      base64: btoa(JSON.stringify(metadata)),
+    });
+
+    await metadataFile.saveIPFS();
+    const metadataURI = metadataFile.ipfs();
+
+    // minting
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+    const feePayerAirdropSignature = await connection.requestAirdrop(
+      keypair.publicKey,
+      LAMPORTS_PER_SOL
+    );
+    await connection.confirmTransaction(feePayerAirdropSignature);
+    const mintNFTResponse = await actions
+      .mintNFT({
+        connection,
+        wallet: new NodeWallet(keypair),
+        uri: metadataURI,
+        maxSupply: 1,
+      })
+      .then((mintNFTResponse) => {
+        alert("Mint successful");
+      });
+  }
   return (
-    <div style={{ margin: "auto auto", width: "40%" }}>
-      {/* <input
-        type="text"
-        name="metadataName"
-        id="metadataName"
-        placeholder="Metadata Name"
-      />
-      <br />
-      <br />
-      <input
-        type="text"
-        name="metadataDescription"
-        id="metadataDescription"
-        placeholder="Metadata Description"
-      />
-      <br />
-      <br />
-      <input type="file" name="fileInput" id="file" placeholder="File" /> */}
-      <Form
-        buttonConfig={{
-          onClick: function noRefCheck() {},
-          theme: "primary",
+    <div
+      style={{
+        margin: "auto auto",
+        width: "40%",
+        border: "1px solid black",
+        height: "400px",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
         }}
-        data={[
-          {
-            inputWidth: "100%",
-            name: "Name of your NFT",
-            type: "text",
-            value: "",
-          },
-          {
-            inputWidth: "100%",
-            name: "NFT Description",
-            type: "textarea",
-            value: "",
-          },
-        ]}
-        onSubmit={function noRefCheck() {}}
-        title="Upload your cool NFT's"
-      />
+      >
+        <Input
+          label="NFT Name"
+          name="NFT Name"
+          onChange={(e) => setNFTName(e.target.value)}
+        />
+        <TextArea
+          label="NFT Description"
+          name="NFT Description"
+          onChange={(e) => setNFTDescription(e.target.value)}
+        />
+        <input type="file" id="file" />
+        <Button
+          id="test-button-primary-large"
+          onClick={uploadAndMintNFT}
+          size="large"
+          text="Mint NFT"
+          theme="primary"
+          type="button"
+        />
+      </div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,90 +1,11 @@
-import { actions, NodeWallet } from "@metaplex/js";
-import {
-  clusterApiUrl,
-  Connection,
-  Keypair,
-  LAMPORTS_PER_SOL,
-} from "@solana/web3.js";
-import { Button } from "web3uikit";
 import Login from "../components/Login";
-import { useState } from "react";
 import NftForm from "../components/NftForm";
 
 function Home() {
-  async function toTheMoon() {
-    // Storing the file
-    console.log("Uploading the image");
-    const keypair = Keypair.generate();
-    console.log(keypair.publicKey.toBase58());
-    let user = Moralis.User.current();
-    const fileInput = document.getElementById("file");
-    const data = fileInput.files[0];
-    const imageFile = new Moralis.File(data.name, data);
-    await imageFile.saveIPFS();
-
-    // Storing the metadata
-
-    const imageURI = imageFile.ipfs();
-    console.log("Image URI: ", imageURI);
-    const metadata = {
-      name: document.getElementById("metadataName").value,
-      symbol: "MLH",
-      description: document.getElementById("metadataDescription").value,
-      image: imageURI,
-      seller_fee_basis_points: 0,
-      properties: {
-        files: [
-          {
-            uri: imageURI,
-            type: "image/jpeg",
-          },
-        ],
-        category: "image",
-        creators: [
-          {
-            address: keypair.publicKey,
-            verified: true,
-            share: 100,
-          },
-        ],
-      },
-    };
-    const metadataFile = new Moralis.File("metadata.json", {
-      base64: btoa(JSON.stringify(metadata)),
-    });
-    console.log("Uploading Metadata");
-    await metadataFile.saveIPFS();
-    const metadataURI = metadataFile.ipfs();
-    console.log(metadataURI);
-    alert("Upload successful");
-
-    // minting
-    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-
-    const feePayerAirdropSignature = await connection.requestAirdrop(
-      keypair.publicKey,
-      LAMPORTS_PER_SOL
-    );
-    await connection.confirmTransaction(feePayerAirdropSignature);
-    const mintNFTResponse = await actions
-      .mintNFT({
-        connection,
-        wallet: new NodeWallet(keypair),
-        uri: metadataURI,
-        maxSupply: 1,
-      })
-      .then((mintNFTResponse) => {
-        console.log(mintNFTResponse);
-        alert("Mint successful");
-      });
-  }
   return (
     <div>
       <Login />
-
       <NftForm />
-
-      <button onClick={toTheMoon}>Upload</button>
     </div>
   );
 }
